40c3abffa6070f3a55f421d5745df8b4eac06fcf
he_wei_guang <heweiguang@zuzuche.com>
2019-04-01 07:13:45 +0800

    字体生成改用 webfonts-generator

--- helper/postcss-plugin-svg2iconfont.js
+++ helper/postcss-plugin-svg2iconfont.js
@@ -5,73 +5,77 @@
 const postcss = require('postcss');
 const path = require('path');
 const fs = require('fs-extra');
-const webFont = require("webfont").default;
-const XXH = require('xxhashjs');
-const types = ['eot', 'ttf', 'woff', 'woff2', 'svg'];
+const webfontsGenerator = require('webfonts-generator');
+const types = ['eot', 'ttf', 'woff', 'woff2'];
 const svgSymbol = '/* @svg2iconfont */';
 
-const generateFontFromSvg = (svgPath, fontClassName, publicPath) => {
+const base64header = type => `data:application/font-${type};charset=utf-8;base64,`;
+
+const generateFontFromSvg = (svgPath, fontName, publicPath) => {
   console.log('--- 开始将 SVG 转为 FONT ---');
 
   return new Promise((resolve, reject) => {
+    const options = {
+      files: [],
+      fontName,
+      writeFiles: false,
+      dest: '',
+      cssTemplate: path.resolve('helper/templates/css.hbs'),
+      types,
+      templateOptions: {
+        classPrefix: 'icons-',
+        baseSelector: '.icons',
+      },
+    }
 
-    webFont({
-      files: path.join(svgPath, '*.svg'),
-      fontName: 'iconfont',
-      template: "css",
-      templateClassName: fontClassName,
-      templateFontPath: publicPath ? `${publicPath}/static/fonts/` : '/'
-    })
-    .then(result => {
-      const {
-        ttf,
-        eot,
-        woff,
-        woff2,
-        // svg,
-        template,
-        config
-      } = result;
+    // 生产环境需要生成文件上传到 CDN，并且加上 publicPath
+    if (publicPath) {
+      // options.writeFiles = true;
+      // options.dest = path.resolve('static/font');
+      // options.cssFontsUrl = publicPath;
+    }
 
-      const folder = path.resolve('static/fonts');
+    // 生成 SVG 文件列表
+    try {
+      const svgArray = fs.readdirSync(svgPath);
 
-      // 生产环境
-      if (publicPath) {
-        if (!fs.existsSync(folder)) {
-          fs.mkdirpSync(folder);
+      svgArray.forEach(svg => {
+        if (path.extname(svg) !== '.svg') {
+          return;
         }
 
-        const fontName = `${config.fontName}-${XXH.h32(0).update(eot).digest()}`;
+        options.files.push(path.join(svgPath, svg));
+      });
+    } catch (error) {
+      reject(error);
+    }
 
-        types.forEach(type => {
-          fs.writeFileSync(path.resolve(path.join(folder, `${fontName}.${type}`)), result[type]);
-        });
-
-        console.log('--- 完成 ---');
-
-        const reg = new RegExp(`([^#])${config.fontName}`, 'g');
-
-        resolve(template.replace(reg, `$1${fontName}`));
-
-        return;
+    webfontsGenerator(options, function(error, result) {
+      if (error) {
+        return reject(error);
       }
 
-      const base64header = 'data:image/png;base64,';
+      const {
+        generateCss
+      } = result;
 
-      const style = template
-      .replace(/([^"]*ttf[^"]*)/g, base64header + ttf.toString('base64'))
-      .replace(/([^"]*eot[^"]*)/g, base64header + eot.toString('base64'))
-      .replace(/([^"]*woff[^"]*)/g, base64header + woff.toString('base64'))
-      .replace(/([^"]*woff2[^"]*)/g, base64header + woff2.toString('base64'));
+      if (publicPath) {
+        console.log('--- 完成将 SVG 转为 FONT ---');
 
-      console.log('--- 完成 ---');
+        return resolve(generateCss());
+      }
 
-      resolve(style);
-    })
-    .catch(error => {
-      reject(error);
+      const urls = {};
+
+      types.forEach(type => {
+        urls[type] = base64header(type) + result[type].toString('base64')
+      });
+
+      console.log('--- 完成将 SVG 转为 FONT ---');
+
+      resolve(generateCss(urls));
     });
-  })
+  });
 }
 
 module.exports = postcss.plugin('svg2iconfont', options => {
